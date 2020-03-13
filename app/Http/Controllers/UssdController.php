ca8dff9122dd20b7179822371c3c9f812d5ee891
theed <edwin@wn.co.ke>
2020-03-13 17:15:51 +0300

    fix ussd amount

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -12,17 +12,44 @@ class UssdController extends Controller
         header('Content-type: text/plain');
         set_time_limit(100);
 
-
         //get inputs
-        $sessionId = $_REQUEST["sessionId"];
-        $serviceCode = $_REQUEST["serviceCode"];
-        $phoneNumber = $_REQUEST["phoneNumber"];
-        $text = trim($_REQUEST["text"]);
+        $sessionId = $_REQUEST["session_id"];
+        $serviceCode = $_REQUEST["service_code"];
+        $phoneNumber = $_REQUEST["mobile_number"];
+        $text = trim($_REQUEST["message"]);
 
-        $data = ['phone' => $phoneNumber, 'text' => $text, 'service_code' => $serviceCode, 'session_id' => $sessionId];
+        $data = ['mobile_number' => $phoneNumber, 'text' => $text, 'service_code' => $serviceCode, 'session_id' => $sessionId];
 
         $payload = new Payload();
-        $payload->data = \GuzzleHttp\json_encode($_REQUEST);
+        $payload->data = \GuzzleHttp\json_encode($data);
         $payload->save();
+
+        $exp_service_code = explode("*",$serviceCode);
+
+        if(count($exp_service_code) > 3){
+            $amount = $exp_service_code[3];
+            self::sendResponse('Please wait for mpesa pin to pay kshs.'.$amount,3);
+        }else{
+
+            self::sendResponse('Please enter *989*100*Amount#',3);
+        }
+
+    }
+
+    public function sendResponse($response,$type=1,$user=null)
+    {
+
+        if ($type == 1) {
+            $output = "CON ";
+        } elseif($type == 2) {
+            $output = "CON ";
+        }else{
+            $output = "END ";
+        }
+
+        $output .= $response;
+        header('Content-type: text/plain');
+        echo $output;
+        exit;
     }
 }
